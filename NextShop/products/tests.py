b704687b49bba8e68f438305b072a122b01d1061
ali-hv <alihv5000@gmail.com>
2024-04-01 06:11:30 +0330

    feat(products): create Product model and an unittest for it

--- NextShop/products/tests.py
+++ NextShop/products/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+class ProductModelTest(TestCase):
+    def test_product_creation(self):
+        product = Product.objects.create(
+            name='Test Product',
+            slug='test-product',
+            description='This is a test product',
+            price=10.00,
+            stock=5
+        )
+        self.assertEqual(product.name, 'Test Product')
+        self.assertEqual(product.slug, 'test-product')
+        self.assertEqual(product.description, 'This is a test product')
+        self.assertEqual(product.price, 10.00)
+        self.assertEqual(product.stock, 5)
