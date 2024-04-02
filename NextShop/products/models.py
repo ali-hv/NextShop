02dfda746efc19c5643ea01199df1577adccfed4
alihv <alihv5000@gmail.com>
2024-04-03 01:29:31 +0330

    feat(Product): add image, created_at and updated_at fields

--- NextShop/products/models.py
+++ NextShop/products/models.py
@@ -8,6 +8,14 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     stock = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def image_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
--- NextShop/products/tests/factories.py
+++ NextShop/products/tests/factories.py
@@ -1,13 +1,34 @@
-from ..models import Product
 import factory
+import os
+
+from ..models import Product
 
 
 class ProductFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Product
+        skip_postgeneration_save = True
 
     name = factory.Faker('sentence', nb_words=4)
     slug = factory.Sequence(lambda n: 'product-{}'.format(n))
     description = factory.Faker('paragraph', nb_sentences=3)
     price = factory.Faker('random_number', digits=2, fix_len=True)
     stock = factory.Faker('random_int', min=0, max=100)
+    image = factory.django.ImageField(filename='test_image.jpg')
+
+    @factory.post_generation
+    def cleanup_files(obj, create, extracted, **kwargs):
+        """
+        Post-generation hook to clean up generated image files after the test.
+        """
+        if not create:
+            # No object was created, do nothing
+            return
+
+        if not obj.image:
+            # No image was generated for this object, do nothing
+            return
+
+        # Delete the generated image file
+        if os.path.isfile(obj.image.path):
+            os.remove(obj.image.path)
