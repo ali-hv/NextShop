1b2a677bfe7dbea0497060d115658e86e8215f7a
alihv <alihv5000@gmail.com>
2024-04-03 00:40:44 +0330

    feat(Product): add Product model to admin panel

--- NextShop/products/admin.py
+++ NextShop/products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+    list_filter = ('name', 'price')
+    search_fields = ('name',)
