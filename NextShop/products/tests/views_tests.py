4fe409478a9976fa54495860db986bada81e44e0
alihv <alihv5000@gmail.com>
2024-04-05 15:17:47 +0330

    test(products): test product DetailView

--- NextShop/products/tests/views_tests.py
+++ NextShop/products/tests/views_tests.py
@@ -25,9 +25,12 @@ class ProductViewsTest(TestCase):
 
         # Check that the response contains the serialized product data
         expected_data = ProductSerializer([self.product1, self.product2], many=True).data
+
+        # Convert image urls to absolute urls in expected data
         for item in expected_data:
             item['image'] = 'http://testserver' + item['image']
 
+        # Check that response data and expected data are equal
         self.assertEqual(response.data, expected_data)
 
     def test_list_products_empty(self):
@@ -42,3 +45,20 @@ class ProductViewsTest(TestCase):
 
         # Check that the response contains an empty list of products
         self.assertEqual(response.data, [])
+
+    def test_detail_product(self):
+        # Make a GET request to DetailProduct view
+        response = self.client.get(reverse("products:product_detail",
+                                           args=[self.product1.id]))
+
+        # Check that the response status code is 200 OK
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Check that the response contains the serialized product data
+        expected_data = ProductSerializer(self.product1).data
+
+        # Convert image url to absolute url in expected data
+        expected_data['image'] = 'http://testserver' + expected_data['image']
+
+        # Check that response data and expected data are equal
+        self.assertEqual(response.data, expected_data)
