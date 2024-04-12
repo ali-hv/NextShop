545de9c27bd0258792801ba8bfe8f2239a0f2e28
alihv <alihv5000@gmail.com>
2024-04-12 19:41:12 +0330

    feat(products): set IsStaffOrReadOnly permission for views

--- NextShop/products/views.py
+++ NextShop/products/views.py
@@ -1,14 +1,17 @@
 from rest_framework import generics
 
 from .serializers import ProductSerializer
+from .permissions import IsStaffOrReadOnly
 from .models import Product
 
 
 class ListProduct(generics.ListCreateAPIView):
+    permission_classes = (IsStaffOrReadOnly,)
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class DetailProduct(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsStaffOrReadOnly,)
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
