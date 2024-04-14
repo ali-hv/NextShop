3af390db95fa52d4c0b845ec1080ecaad605a5ca
alihv <alihv5000@gmail.com>
2024-04-15 00:37:54 +0330

    feat(accounts): set permissions for views

---
+++ NextShop/accounts/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework import permissions
+
+
+class IsOwnerOrAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj or \
+        (request.user.is_superuser and request.method in permissions.SAFE_METHODS)
--- NextShop/accounts/views.py
+++ NextShop/accounts/views.py
@@ -1,14 +1,19 @@
 from django.contrib.auth import get_user_model
+
+from rest_framework.permissions import IsAdminUser
 from rest_framework import generics
 
+from .permissions import IsOwnerOrAdmin
 from .serializers import UserSerializer
 
 
 class UserList(generics.ListCreateAPIView):
+    permission_classes = (IsAdminUser,)
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrAdmin,)
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
