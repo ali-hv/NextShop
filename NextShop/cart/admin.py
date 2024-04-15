b4f786a328dbc837e609576a635af9447dd61130
alihv <alihv5000@gmail.com>
2024-04-15 23:25:31 +0330

    feat(cart): create CartItem model

--- NextShop/cart/admin.py
+++ NextShop/cart/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Cart
+from .models import Cart, CartItem
 
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    list_display = ('user',)
+    list_display = ('user', 'total_payable_price',)
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity', 'total_price',)
--- NextShop/cart/models.py
+++ NextShop/cart/models.py
@@ -1,14 +1,35 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+from decimal import Decimal
+
 from products.models import Product
 
 
 class Cart(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name="cart")
-    products = models.ManyToManyField(Product, blank=True)
 
-    def get_payable_amount(self):
-        # Calculate the total price of cart using aggregation
-        total_price = self.products.aggregate(total=models.Sum('price'))['total']
-        return total_price or 0  # Return 0 if total_price is None
+    def __str__(self):
+        return self.user.username
+
+    @property
+    def total_payable_price(self):
+        # Calculate the total price for each item and sum up the results
+        total_price = sum(item.total_price for item in self.items.all())
+        return total_price
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        unique_together = ('cart', 'product')  # Ensure only one instance of each product per cart
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Cart for {self.cart.user}"
+
+    @property
+    def total_price(self):
+        return self.product.price * Decimal(self.quantity)
